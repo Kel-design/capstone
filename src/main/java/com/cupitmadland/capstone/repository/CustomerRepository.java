721700c991eb76e0871a1cd2e9ee4fcf1895d7a1
Kelly Madland <kellymadland@gmail.com>
2023-12-19 08:05:47 -0600

    customer repository custom query

--- src/main/java/com/cupitmadland/capstone/repository/CustomerRepository.java
+++ src/main/java/com/cupitmadland/capstone/repository/CustomerRepository.java
@@ -4,4 +4,7 @@ import com.cupitmadland.capstone.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    //custom query to find Customer by email
+    Customer findByEmail(String email);
 }
