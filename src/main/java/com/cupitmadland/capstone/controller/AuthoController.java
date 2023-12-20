0a3fc60f54d00f09f6177c3ef22d9b8deda87378
Kelly Madland <kellymadland@gmail.com>
2023-12-20 17:50:19 -0600

    Added @GetMappings and @PostMapping to AuthoController

--- src/main/java/com/cupitmadland/capstone/controller/AuthoController.java
+++ src/main/java/com/cupitmadland/capstone/controller/AuthoController.java
@@ -1,10 +1,17 @@
 package com.cupitmadland.capstone.controller;
 
+import com.cupitmadland.capstone.entity.Customer;
 import com.cupitmadland.capstone.service.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class AuthoController {
@@ -33,6 +40,42 @@ public class AuthoController {
 
     @GetMapping("/contact_us")
     public String contactUs() { return "contact_us"; }
+
+    @GetMapping("/login")
+    public String login() { return "login"; }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        Customer customer = new Customer();
+        model.addAttribute("CandleCustomer", customer);
+        return "register";
+    }
+
+    @PostMapping("register/save")
+    public String registration(@Valid @ModelAttribute("CandleCustomer") Customer customer, BindingResult result, Model model){
+        Customer existingCandleCustomer = customerService.findCustomerByEmail(customer.getEmail());
+
+        if (existingCandleCustomer != null && existingCandleCustomer.getEmail() != null && !existingCandleCustomer.getEmail().isEmpty()){
+            result.rejectValue("email", null, "There is already an account registered with the same email");
+        }
+        if (result.hasErrors()){
+            model.addAttribute("CandleCustomer", customer);
+            return "/register";
+        }
+        customerService.saveCustomer(customer);
+        return "redirect:/register?success";
+    }
+
+    @GetMapping("/newsletter")
+    public String customer(Model model){
+        List<Customer> customerList = customerService.findAllCustomer();
+
+        model.addAttribute("customer", customerList);
+
+        return "newsletter";
+    }
+
+
 
 
 
