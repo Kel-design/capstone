263c7b71e54af9439aad69dbbfc03f969d63e70f
Kelly Madland <kellymadland@gmail.com>
2023-12-20 17:52:07 -0600

    Customer Service added custom implementations

--- src/main/java/com/cupitmadland/capstone/service/CustomerService.java
+++ src/main/java/com/cupitmadland/capstone/service/CustomerService.java
@@ -2,7 +2,13 @@ package com.cupitmadland.capstone.service;
 
 import com.cupitmadland.capstone.entity.Customer;
 
+import java.util.List;
+
 public interface CustomerService {
 
     void saveCustomer(Customer customer);
+
+    Customer findCustomerByEmail(String email);
+
+    List<Customer> findAllCustomer();
 }
