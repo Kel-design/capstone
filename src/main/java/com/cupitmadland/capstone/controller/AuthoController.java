1f348c79655585f80c2ff7765a806e090d968e03
Kelly Madland <kellymadland@gmail.com>
2023-12-20 11:01:26 -0600

    AuthoController @GetMappings for page endpoints

--- src/main/java/com/cupitmadland/capstone/controller/AuthoController.java
+++ src/main/java/com/cupitmadland/capstone/controller/AuthoController.java
@@ -14,5 +14,27 @@ public class AuthoController {
     public AuthoController(CustomerService  customerService) {this.customerService = customerService;}
 
     @GetMapping("/home")
-    public String home(){ return "home"; }
+    public String home() { return "home"; }
+
+    @GetMapping("/candles")
+    public String candles() { return "candles"; }
+
+    @GetMapping("/scents")
+    public String scents() { return "scents"; }
+
+    @GetMapping("/single_product")
+    public String singleProduct() { return "single_product"; }
+
+    @GetMapping("/shopping_cart")
+    public String shoppingCart() { return "shopping_cart"; }
+
+    @GetMapping("/our_story")
+    public String ourStory() { return "our_story"; }
+
+    @GetMapping("/contact_us")
+    public String contactUs() { return "contact_us"; }
+
+
+
+
 }
