95d47acdfcdc324cac6afd9b7819cb0321979f64
Kelly Madland <kellymadland@gmail.com>
2023-12-20 17:49:31 -0600

    Spring Security filterchain updates

--- src/main/java/com/cupitmadland/capstone/config/SpringSecurity.java
+++ src/main/java/com/cupitmadland/capstone/config/SpringSecurity.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -27,11 +28,25 @@ public class SpringSecurity {
                                     .requestMatchers("/our_story").permitAll()
                                     .requestMatchers("/contact_us").permitAll()
                                     .requestMatchers("/shopping_cart").permitAll()
+                                    .requestMatchers("/register/**").permitAll()
+                                    .requestMatchers("/login").permitAll()
+                                    .requestMatchers("/newsletter").hasRole("USER")
+                                    .requestMatchers("/images/**").permitAll().anyRequest().authenticated()
 
 
                     //continue building security chain AND form login in AND logout
 
-                    );
+                    )
+                    .formLogin(
+                            form -> form
+                                    .loginPage("/login")
+                                    .loginProcessingUrl("/login")
+                                    .defaultSuccessUrl("/newsletter")
+                                    .permitAll()
+                    ).logout(
+                            logout -> logout
+                                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                    .permitAll());
 
 return http.build();
     }
