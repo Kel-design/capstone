fcd26b3521117818d87d105bfab53dcf020733d9
Kelly Madland <kellymadland@gmail.com>
2023-12-22 09:05:36 -0600

    created ProductServiceImpl

---
+++ src/main/java/com/cupitmadland/capstone/service/impl/ProductServiceImpl.java
@@ -0,0 +1,66 @@
+package com.cupitmadland.capstone.service.impl;
+
+import com.cupitmadland.capstone.entity.Product;
+import com.cupitmadland.capstone.exception.ProductNotFoundException;
+import com.cupitmadland.capstone.repository.*;
+import com.cupitmadland.capstone.service.ProductService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private CustomerRepository customerRepository;
+
+    private CustOrderRepository custOrderRepository;
+
+    private PaymentRepository paymentRepository;
+
+    private ProductRepository productRepository;
+
+    private RoleRepository roleRepository;
+
+    private ShoppingCartRepository shoppingCartRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    public ProductServiceImpl(CustomerRepository customerRepository, CustOrderRepository custOrderRepository, PaymentRepository paymentRepository,
+                              ProductRepository productRepository, RoleRepository roleRepository, ShoppingCartRepository shoppingCartRepository,
+                              PasswordEncoder passwordEncoder) {
+        super();
+        this.customerRepository = customerRepository;
+        this.custOrderRepository = custOrderRepository;
+        this.paymentRepository = paymentRepository;
+        this.productRepository = productRepository;
+        this.roleRepository = roleRepository;
+        this.shoppingCartRepository = shoppingCartRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void saveProduct(Product product) {
+//write code here!
+    }
+
+    @Override
+    public Product findProductById(Long id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+
+        //checking if product exists
+        if (optionalProduct.isPresent()){
+            return optionalProduct.get();
+        }else{
+            //Handle if product with given ID is not found
+            throw new ProductNotFoundException("Product not found with ID: " + id);
+        }
+    }
+
+    @Override
+    public List<Product> findAllProduct() {
+        List<Product> productList = productRepository.findAll();
+        return productList;
+    }
+}
