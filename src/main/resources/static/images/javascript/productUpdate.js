f6cf0452938db703a5f8a39bf2c4979dc8b6f14f
Kelly Madland <kellymadland@gmail.com>
2023-12-24 08:39:11 -0600

    js file updated & commented out (resolve single_product error trial)

--- src/main/resources/static/images/javascript/productUpdate.js
+++ src/main/resources/static/images/javascript/productUpdate.js
@@ -1,22 +1,23 @@
 
-function updateProduct(size){
+function updateProduct(productName, productPrice, productDescription, imageUrl, size) {
 
     //Trying to use Thymeleaf to get product details dynamically based on size
-    var productName = [[${product.name}]];
-    var productPrice = [[${product.price}]];
-    var productDescription = [[${product.description}]];
+    //let productName = [[${product.name}]];
+    //let productPrice = [[${product.price}]];
+    //let productDescription = [[${product.description}]];
 
     //Determine the appropriate image file based on the selected size
-    var imageUrl = size === 'Large' ? '../static/images/large_grey_candle.jpg' :
-            (size === 'Medium' ? '../static/images/medium_clear_candle.jpg' :
-            (size === 'Small' ? '../static/images/small_black_candle.jpg' : '../static/images/large_grey_candle.jpg' ));
+    //let imageUrl = size === 'Large' ? '/static/images/large_grey_candle.jpg' :
+        //(size === 'Medium' ? '/static/images/medium_clear_candle.jpg' :
+           // (size === 'Small' ? '/static/images/small_black_candle.jpg' : '/static/images/large_grey_candle.jpg'));
 
 
     document.getElementById('productName').innerText = productName;
     document.getElementById('productPrice').innerText = productPrice;
     document.getElementById('productDescription').innerText = productDescription;
-    document.getElementById('productImage').src = imageUrl;
 
-
-
+    let productImage = document.getElementById('productImage');
+    if (productImage) {
+    productImage.src = imageUrl;
+    }
 }
