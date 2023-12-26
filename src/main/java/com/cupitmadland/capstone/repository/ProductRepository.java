531e1f1474f0707c3eec02197f5660f4a443fac7
Kelly Madland <kellymadland@gmail.com>
2023-12-26 15:12:09 -0600

    ProductRepository add custom query to find by size and scent

--- src/main/java/com/cupitmadland/capstone/repository/ProductRepository.java
+++ src/main/java/com/cupitmadland/capstone/repository/ProductRepository.java
@@ -3,5 +3,10 @@ package com.cupitmadland.capstone.repository;
 import com.cupitmadland.capstone.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //Custom query method to find a product by size
+    Optional<Product> findBySizeAndScent(String size, String scent);
 }
