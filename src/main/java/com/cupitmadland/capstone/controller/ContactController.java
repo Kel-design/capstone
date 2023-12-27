70255fc8d1a8445b4045d8c685d4fe40df284ecf
Kelly Madland <kellymadland@gmail.com>
2023-12-27 17:01:13 -0600

    contact us function updates

---
+++ src/main/java/com/cupitmadland/capstone/controller/ContactController.java
@@ -0,0 +1,45 @@
+package com.cupitmadland.capstone.controller;
+
+import com.cupitmadland.capstone.entity.Customer;
+import com.cupitmadland.capstone.service.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+
+
+    private final CustomerService customerService;
+    @Autowired
+    public ContactController(CustomerService customerService) {this.customerService = customerService;}
+
+    @GetMapping("/contact_us")
+    public String showContactForm(Model model) {
+        model.addAttribute("contactInfo", new Customer());
+        return "contact_us";
+    }
+
+    @PostMapping("/contact_us/save")
+    public String saveContactInfo(@Valid @ModelAttribute("contactInfo") Customer contactInfo, BindingResult result, Model model){
+        // Validate form input
+        if (result.hasErrors()){
+            model.addAttribute("contactInfo", contactInfo);
+            return "contact_us";
+        }
+
+        // Save the contact information to the database
+        customerService.saveCustomer(contactInfo);
+        return "redirect:/thank_you";
+    }
+@RequestMapping("/thank_you")
+    public String showThankYouPage(){
+        return "thank_you";
+    }
+}
