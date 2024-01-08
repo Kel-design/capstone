268a3a65722b35b680513fb59ed5c5fd667ca899
Kelly Madland <kellymadland@gmail.com>
2024-01-08 12:56:14 -0600

    added javadocs for Payment entity

--- src/main/java/com/cupitmadland/capstone/entity/Payment.java
+++ src/main/java/com/cupitmadland/capstone/entity/Payment.java
@@ -8,34 +8,61 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.List;
 
-// Entity to hold Customer payment information.
+/**
+ * Entity class representing a Payment, holding customer payment information.
+ */
 @Entity
 @Data
 @NoArgsConstructor
 public class Payment {
 
+    /**
+     * The unique identifier for the Payment.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    /**
+     * The timestamp of the payment.
+     */
     Timestamp paymentDate;
 
+    /**
+     * The total amount of the payment.
+     */
     BigDecimal totalAmount;
 
+    /**
+     * The credit card number used for the payment.
+     */
     String creditCardNum;
 
+    /**
+     * The expiration date of the credit card.
+     */
     String expDate;
 
+    /**
+     * The security code of the credit card.
+     */
     String securityCode;
 
+    /**
+     * The name on the credit card.
+     */
     String nameOnCard;
 
-    //Creating a @ManyToOne entity relationship for Payment to Customer using customer_id
+    /**
+     * The Customer associated with the Payment.
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id")
     private Customer customer;
 
-    // Creating a @OneToMany entity relation for Payment to CartItem
+    /**
+     * The list of CartItems associated with the Payment.
+     */
     @OneToMany(mappedBy = "payment", cascade = CascadeType.ALL)
     private List<CartItem> cartItems;
 
