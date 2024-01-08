d56f18bd5fa19686e01c4fb52fcca13826429103
Kelly Madland <kellymadland@gmail.com>
2024-01-08 12:14:26 -0600

    added javadocs for CartItemDTO

--- src/main/java/com/cupitmadland/capstone/DTO/CartItemDTO.java
+++ src/main/java/com/cupitmadland/capstone/DTO/CartItemDTO.java
@@ -3,13 +3,22 @@ package com.cupitmadland.capstone.DTO;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// Used to help hold Cart Item data while using it to update cart, checkout and show order details.
+/**
+ * Data Transfer Object (DTO) class representing Cart Item data.
+ * Used to hold Cart Item data while updating the cart, processing checkout and displaying order details.
+ */
 @Data
 @NoArgsConstructor
 public class CartItemDTO {
 
+    /**
+     * The ID of the product associated with the Cart Item.
+     */
     public Long productId;
 
+    /**
+     * The quantity of the product in the Cart Item.
+     */
     public Integer productQuantity;
 
 }
