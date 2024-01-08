833d8d78393eab689fa70232643499779e6f407c
Kelly Madland <kellymadland@gmail.com>
2024-01-08 13:42:25 -0600

    added javadocs to RoleRepository

--- src/main/java/com/cupitmadland/capstone/repository/RoleRepository.java
+++ src/main/java/com/cupitmadland/capstone/repository/RoleRepository.java
@@ -3,7 +3,18 @@ package com.cupitmadland.capstone.repository;
 import com.cupitmadland.capstone.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing Role entities.
+ * Extends JpaRepository providing basic CRUD operations.
+ *
+ *  @see org.springframework.data.jpa.repository.JpaRepository
+ */
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    /**
+     * Finds a role by its name.
+     * @param name The name of the role to find.
+     * @return The Role entity if found, null otherwise.
+     */
     Role findByName(String name);
 }
