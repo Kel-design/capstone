00665aa48fc2601191d2f030d34fed71dbecbcd3
Kelly Madland <kellymadland@gmail.com>
2024-01-08 13:28:16 -0600

    added javadocs to CustOrdeRepository

--- src/main/java/com/cupitmadland/capstone/repository/CustOrderRepository.java
+++ src/main/java/com/cupitmadland/capstone/repository/CustOrderRepository.java
@@ -3,5 +3,12 @@ package com.cupitmadland.capstone.repository;
 import com.cupitmadland.capstone.entity.CustOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+/**
+ * Repository interface for managing CustOrder entities in the database.
+ * Extends JpaRepository providing basic CRUD operations.
+ *
+ * @see org.springframework.data.jpa.repository.JpaRepository
+ */
 public interface CustOrderRepository extends JpaRepository<CustOrder, Long> {
 }
