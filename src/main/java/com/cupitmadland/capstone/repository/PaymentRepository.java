dabf342292491e10a6c01886120017a7cb66a867
Kelly Madland <kellymadland@gmail.com>
2024-01-08 13:30:12 -0600

    added javadocs to PaymentRepository

--- src/main/java/com/cupitmadland/capstone/repository/PaymentRepository.java
+++ src/main/java/com/cupitmadland/capstone/repository/PaymentRepository.java
@@ -3,5 +3,11 @@ package com.cupitmadland.capstone.repository;
 import com.cupitmadland.capstone.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing Payment entities in the database.
+ * Extends JpaRepository providing basic CRUD operations.
+ *
+ * @see org.springframework.data.jpa.repository.JpaRepository
+ */
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
