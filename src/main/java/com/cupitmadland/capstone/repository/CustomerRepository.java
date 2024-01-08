88e512a9694883ec3b7b37c9ec7fe276d301248e
Kelly Madland <kellymadland@gmail.com>
2024-01-08 13:25:15 -0600

    added javadocs to CustomerRepository

--- src/main/java/com/cupitmadland/capstone/repository/CustomerRepository.java
+++ src/main/java/com/cupitmadland/capstone/repository/CustomerRepository.java
@@ -3,8 +3,19 @@ package com.cupitmadland.capstone.repository;
 import com.cupitmadland.capstone.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing Customer entities in the database.
+ * Extends JpaRepository providing basic CRUD operations.
+ *
+ * @see org.springframework.data.jpa.repository.JpaRepository
+ */
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    //custom query to find Customer by email
+    
+    /**
+     * Custom query to find a Customer by email.
+     *
+     * @param email the email address of the Customer to be retrieved
+     * @return the Customer entity if found, or null if not found
+     */
     Customer findByEmail(String email);
 }
