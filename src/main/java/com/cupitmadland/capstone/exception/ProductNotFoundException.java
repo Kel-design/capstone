fad014be3da34574eb1355cda3bc8526112f53bb
Kelly Madland <kellymadland@gmail.com>
2024-01-08 13:18:30 -0600

    added javadoc to ProductNotFoundException custom exception

--- src/main/java/com/cupitmadland/capstone/exception/ProductNotFoundException.java
+++ src/main/java/com/cupitmadland/capstone/exception/ProductNotFoundException.java
@@ -1,7 +1,17 @@
 package com.cupitmadland.capstone.exception;
 
 // Used to throw exception when a product is not found.
+
+/**
+ * Custom exception class for handling cases when a product is not found.
+ */
 public class ProductNotFoundException extends RuntimeException{
+
+    /**
+     * Constructs a new ProductNotFoundException with the specified detail message.
+     * 
+     * @param message the detail message saved when called
+     */
     public ProductNotFoundException(String message){
         super(message);
     }
