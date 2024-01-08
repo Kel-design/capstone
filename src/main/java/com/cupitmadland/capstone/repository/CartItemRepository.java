247aa0d1a8b2e3185e84f70e4d2b4e05e4bb9ced
Kelly Madland <kellymadland@gmail.com>
2024-01-08 13:21:43 -0600

    added javadocs for CartItemRepository

--- src/main/java/com/cupitmadland/capstone/repository/CartItemRepository.java
+++ src/main/java/com/cupitmadland/capstone/repository/CartItemRepository.java
@@ -3,5 +3,11 @@ package com.cupitmadland.capstone.repository;
 import com.cupitmadland.capstone.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing CartItem entities in the database.
+ * Extends JpaRepository providing basic CRUD operations.
+ *
+ * @see org.springframework.data.jpa.repository.JpaRepository
+ */
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
