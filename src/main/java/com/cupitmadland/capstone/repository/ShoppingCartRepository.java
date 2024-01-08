5c8a757e119fc3a6d9e6b1a0f268e49fe8fc60a5
Kelly Madland <kellymadland@gmail.com>
2024-01-08 13:43:55 -0600

    added javadocs for ShoppingCartRepository

--- src/main/java/com/cupitmadland/capstone/repository/ShoppingCartRepository.java
+++ src/main/java/com/cupitmadland/capstone/repository/ShoppingCartRepository.java
@@ -3,5 +3,11 @@ package com.cupitmadland.capstone.repository;
 import com.cupitmadland.capstone.entity.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ *  Repository interface for managing ShoppingCart entities in the database.
+ *  Extends JpaRepository providing basic CRUD operations.
+ *
+ *  @see org.springframework.data.jpa.repository.JpaRepository
+ */
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
 }
