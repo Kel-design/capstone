d405659e18963ee8879ea4319dbb4d9486bb84ff
Kelly Madland <kellymadland@gmail.com>
2024-01-08 12:47:15 -0600

    added javadocs to Customer entity

--- src/main/java/com/cupitmadland/capstone/entity/Customer.java
+++ src/main/java/com/cupitmadland/capstone/entity/Customer.java
@@ -7,67 +7,116 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
-// Entity to hold Customer information
+/**
+ * Entity class representing a Customer and holding customer information.
+ */
 @Entity
 @Data
 @NoArgsConstructor
 public class Customer {
 
+    /**
+     * The unique identifier for the Customer.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    /**
+     * The first name of the Customer.
+     */
     String firstName;
 
+    /**
+     * The middle name of the Customer.
+     */
     String middleName;
 
+    /**
+     * The last name of the Customer.
+     */
     String lastName;
 
+    /**
+     * The address of the Customer.
+     */
     String address;
 
+    /**
+     * The city of the Customer.
+     */
     String city;
 
+    /**
+     * The state of the Customer.
+     */
     String state;
 
+    /**
+     * The ZIP Code of the Customer.
+     */
     String zipCode;
 
+    /**
+     * The phone number of the Customer.
+     */
     String phone;
 
+    /**
+     * The email address of the Customer.
+     */
     String email;
 
+    /**
+     * The contact us message associated with the Customer (limited to 1000 characters).
+     */
     @Column(name = "message", length = 1000)
     String message;
 
+    /**
+     * The username associated with the Customer.
+     */
     @Column(nullable = true)
     String username;
 
+    /**
+     * The password associated with the Customer.
+     */
     @Column(nullable = true, unique = true)
     String password;
 
-    //Creating a @OneToOne entity relationship for Customer to Shopping Cart using shoppingCartId
+    /**
+     * The Shopping Cart associated with the Customer.
+     */
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "shoppingCartId")
     private ShoppingCart shoppingCart;
 
-    //Creating a @OneToMany entity relationship for Customer to CustOrder using customer_id
-    //@OneToMany(fetch = FetchType.LAZY, mappedBy = "customer")
-    //private List<CustOrder> custOrderList;
 
-    //Creating a @OneToMany entity relationship for Customer to Payment using customer_id
+    /**
+     * The list of Payments associated with the Customer.
+     */
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer", cascade = CascadeType.ALL)
     private List<Payment> paymentList;
 
-    //Creating a @ManyToMany entity relationship for Customer to Role using customer_id and role_id
+    /**
+     * The list of Roles associated with the Customer.
+     */
     @ManyToMany (fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "customer_roles", joinColumns = {@JoinColumn(name = "customer_id", referencedColumnName = "id")},
     inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
     public List<Role> roles = new ArrayList<>();
 
-    // Creating a bidirectional @OneToMany relationship for Customer to CartItem
+    /**
+     * The list of CartItems associated with the Customer.
+     */
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer")
     private List<CartItem> cartItems = new ArrayList<>();
-
-    // New method to add a CartItem to the list and set the customer reference
+    
+    /**
+     * Adds a list of CartItems to the Customer's cartItems list and sets the customer reference for each CartItem.
+     * @param cartItems The list of CartItems to be associated with the Customer.
+     */
     public void addCartItems(List<CartItem> cartItems) {
         cartItems.forEach(cartItem -> {
             cartItem.setCustomer(this);
