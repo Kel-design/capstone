65295d4e3d4ef2d7a9b956aa7bdf1369e88bd142
Kelly Madland <kellymadland@gmail.com>
2024-01-08 14:40:03 -0600

    added javadocs for ShoppingCartService

--- src/main/java/com/cupitmadland/capstone/service/ShoppingCartService.java
+++ src/main/java/com/cupitmadland/capstone/service/ShoppingCartService.java
@@ -6,12 +6,32 @@ import jakarta.servlet.http.HttpSession;
 
 import java.util.ArrayList;
 import java.util.List;
-// Used to add cart items to cart, get a cart item list and transition guest cart to customer cart
+
+
+/**
+ * Service interface for managing shopping cart related operations.
+ */
 public interface ShoppingCartService {
 
+    /**
+     * Adds a cart item to the shopping cart.
+     *
+     * @param cartItem The cart item to be added.
+     */
     void addToCart(CartItem cartItem);
 
+    /**
+     * Retrieves a list of cart items in the shopping cart.
+     *
+     * @return The list of cart items.
+     */
     List<CartItem> getCartItemList();
 
+    /**
+     * Adds guest cart items to the customer's cart.
+     *
+     * @param customer The customer for whom the cart items will be added.
+     * @param session The HttpSession containing guest cart items.
+     */
     public void addGuestCartToCustomerCart(Customer customer, HttpSession session);
 }
