d0b62fc32f465a2b6dea699849618e7bc3e9c8b4
Kelly Madland <kellymadland@gmail.com>
2024-01-08 14:27:54 -0600

    added javadocs for CustomerService

--- src/main/java/com/cupitmadland/capstone/service/CustomerService.java
+++ src/main/java/com/cupitmadland/capstone/service/CustomerService.java
@@ -4,11 +4,30 @@ import com.cupitmadland.capstone.entity.Customer;
 
 import java.util.List;
 
+/**
+ * Service interface for managing customer-related operations.
+ */
 public interface CustomerService {
 
+    /**
+     * Saves a customer to the database.
+     *
+     * @param customer The customer to be saved.
+     */
     void saveCustomer(Customer customer);
 
+    /**
+     * Finds a customer by email.
+     *
+     * @param email The email of the customer to be found.
+     * @return The found customer or null if not found.
+     */
     Customer findCustomerByEmail(String email);
 
+    /**
+     * Retrieves a list of all customers.
+     *
+     * @return A list of all customers.
+     */
     List<Customer> findAllCustomer();
 }
