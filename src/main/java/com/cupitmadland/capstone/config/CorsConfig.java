6018b9a7d51b6c8beb0083b1a79dff5fb9ccefa2
Kelly Madland <kellymadland@gmail.com>
2024-01-08 12:03:22 -0600

    added javadocs to CorsConfig

--- src/main/java/com/cupitmadland/capstone/config/CorsConfig.java
+++ src/main/java/com/cupitmadland/capstone/config/CorsConfig.java
@@ -4,8 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// Initially created when trying to use AJAX to move single product data to shopping cart.
-// Switched strategies to DTO & HTTP session.
+
+/**
+ * Configuration class to enable Cross-Origin Resource Sharing (CORS) for specific endpoints.
+ * Used initially when attempting to use AJAX to move single product data to the shopping cart.
+ * However, the strategy was later switched to DTP and HTTP session.
+ * Keeping this here to possibly use in future updates.
+ */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
