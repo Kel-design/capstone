6853e8cbe70184e68d9ec960b607c2b7353916d3
Kelly Madland <kellymadland@gmail.com>
2024-01-08 14:35:00 -0600

    added javadocs to ProductService

--- src/main/java/com/cupitmadland/capstone/service/ProductService.java
+++ src/main/java/com/cupitmadland/capstone/service/ProductService.java
@@ -5,15 +5,42 @@ import com.cupitmadland.capstone.entity.Product;
 import java.util.List;
 
 // Used to get product information both by id and by size and scent
+
+/**
+ * Service interface for managing product-related operations.
+ */
 public interface ProductService {
 
+    /**
+     * Future use: Saves a product to the database.
+     *
+     * @param product The product to be saved.
+     */
     void saveProduct(Product product);
+
+    /**
+     * Finds a product by its ID.
+     *
+     * @param id The ID of the product to be found.
+     * @return The found product or null if not found.
+     */
     Product findProductById(Long id);
 
+    /**
+     * Future use: Retrieves a lise of all products.
+     *
+     * @return A list of all products.
+     */
     List<Product> findAllProduct();
 
-    //New method to find a product by size AND scent
+
+    /**
+     * Finds a product by its size and scent.
+     *
+     * @param size The size of the product.
+     * @param scent The scent of the product.
+     * @return The found product or null if not found.
+     */
     Product findProductBySizeAndScent(String size, String scent);
-
-
+    
 }
